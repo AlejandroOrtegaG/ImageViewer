bae6bcc373cd4306033d84de61f673d0c54e6d91
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-01-21 21:04:01 +0000

    All interfaces implemented

--- src/main/java/imageViewer/ImageDisplay.java
+++ src/main/java/imageViewer/ImageDisplay.java
@@ -1,4 +1,23 @@
 package imageViewer;
 
 public interface ImageDisplay {
+
+    void paint(String id, int offset);
+    int getWidth();
+    void clear();
+    void on(Shift shift);
+    void on(Released released);
+
+    interface Shift {
+
+        Shift Null = offset -> {};
+        void offset(int offset);
+
+    }
+    interface Released {
+
+        Released Null = offset -> {};
+        void offset(int offset);
+
+    }
 }
